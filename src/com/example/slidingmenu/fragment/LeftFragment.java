86e90e8ac46a9bf225120bcfbd06e513faea855c
ning.Zhang <ning.zhang@inno-optics.com>
2013-09-21 08:37:04 +0800

    modify lift Signed-off-by: ning.Zhang <ning.zhang@inno-optics.com>

--- src/com/example/slidingmenu/fragment/LeftFragment.java
+++ src/com/example/slidingmenu/fragment/LeftFragment.java
@@ -15,24 +15,99 @@
  */
 package com.example.slidingmenu.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.slidingmenu.R;
+import com.example.slidingmenu.activity.SlidingActivity;
 
 public class LeftFragment extends Fragment {
 
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.left, null);
-		return view;
-	}
+    TextView tuFa, kaoQin, mMessage, mContact, personal_center, mConfig;
 
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-	}
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left, null);
+
+        initView(view);
+        return view;
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+
+    private void initView(View view) {
+
+        tuFa = (TextView) view.findViewById(R.id.tv_left_tu_fa);
+        kaoQin = (TextView) view.findViewById(R.id.tv_left_kao_qin);
+        mMessage = (TextView) view.findViewById(R.id.tv_left_xiao_xi);
+        mContact = (TextView) view.findViewById(R.id.tv_left_contact);
+        personal_center = (TextView) view.findViewById(R.id.tv_left_person_center);
+        mConfig = (TextView) view.findViewById(R.id.tv_left_config);
+
+        tuFa.setOnClickListener(listener);
+        kaoQin.setOnClickListener(listener);
+        mMessage.setOnClickListener(listener);
+        mContact.setOnClickListener(listener);
+        personal_center.setOnClickListener(listener);
+        mConfig.setOnClickListener(listener);
+
+    }
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            Intent intent = new Intent();
+            switch (view.getId()) {
+                case R.id.tv_left_tu_fa:
+                    intent.setClass(getActivity(), SlidingActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+
+                    break;
+                case R.id.tv_left_kao_qin:
+                    intent.setClass(getActivity(), SlidingActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+
+                    break;
+                case R.id.tv_left_xiao_xi:
+                    intent.setClass(getActivity(), SlidingActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+
+                    break;
+                case R.id.tv_left_contact:
+                    intent.setClass(getActivity(), SlidingActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+
+                    break;
+                case R.id.tv_left_person_center:
+                    intent.setClass(getActivity(), SlidingActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+
+                    break;
+                case R.id.tv_left_config:
+                    intent.setClass(getActivity(), SlidingActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+
+                    break;
+            }
+
+
+        }
+    };
 
 }
