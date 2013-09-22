2501c18093a15ea7216d504bd04a3f44bae76d22
genglei <571618791@qq.com>
2013-09-22 21:35:01 +0800

    Signed-off-by: genglei <571618791@qq.com>实现打电话，发短信功能 Signed-off-by: genglei <571618791@qq.com>

--- src/com/example/slidingmenu/tool/ListAdapter.java
+++ src/com/example/slidingmenu/tool/ListAdapter.java
@@ -1,18 +1,21 @@
 package com.example.slidingmenu.tool;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 import com.example.slidingmenu.R;
+import com.example.slidingmenu.fragment.RightFragment;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.example.slidingmenu.R.id.message;
 
 /**
  * Created by Administrator on 13-9-21.
@@ -22,6 +25,7 @@ public class ListAdapter extends BaseAdapter {
 
     private LayoutInflater layoutInflater;
     private View.OnClickListener onClickListener;
+
     private Context mContext;
     private String[] stringArr;
     private String[] phoneArr;
@@ -34,6 +38,7 @@ public class ListAdapter extends BaseAdapter {
         this.onClickListener = listener;
         this.mContext = context;
         stringArr = arr;
+
         this.map = map;
         this.map2 = map2;
         this.phoneArr = phoneArr;
@@ -62,42 +67,67 @@ public class ListAdapter extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder = null;
-//        if (convertView == null) {
-            convertView = layoutInflater.inflate(R.layout.listitem, null);
-            View view = layoutInflater.inflate(R.layout.addview, null);
-            final LinearLayout tv = (LinearLayout) view.findViewById(R.id.add);
+
+        convertView = layoutInflater.inflate(R.layout.listitem, null);
+        View view = layoutInflater.inflate(R.layout.addview, null);
+        final LinearLayout tv = (LinearLayout) view.findViewById(R.id.add);
 
 
-            final LinearLayout layout = (LinearLayout) convertView.findViewById(R.id.linear);
-            layout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (isShow == false) {
-                        layout.removeView(tv);
-                        layout.addView(tv);
-                        isShow = true;
+        final LinearLayout layout = (LinearLayout) convertView.findViewById(R.id.linear);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isShow == false) {
+                    layout.removeView(tv);
+                    layout.addView(tv);
+                    isShow = true;
 
-                    } else if (isShow == true) {
-                        layout.removeView(tv);
-                        isShow = false;
-                    }
-
-
+                } else if (isShow == true) {
+                    layout.removeView(tv);
+                    isShow = false;
                 }
-            });
 
 
-            holder = new ViewHolder();
+            }
+        });
 
-            holder.nameTextView = (TextView) convertView.findViewById(R.id.text_website_name);
-            holder.phoneTextView = (TextView) convertView.findViewById(R.id.text_website_phone);
+        holder = new ViewHolder();
+
+        holder.nameTextView = (TextView) convertView.findViewById(R.id.text_website_name);
+        holder.phoneTextView = (TextView) convertView.findViewById(R.id.text_website_phone);
 
 
-            convertView.setTag(holder);
-//        }
-        /*else {
-            holder = (ViewHolder) convertView.getTag();
-        }*/
+        ImageView call = (ImageView) view.findViewById(R.id.phone);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phonenumber = "10001";
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel://" +phonenumber));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(intent);
+
+            }
+        });
+
+
+
+
+        ImageView send = (ImageView) view.findViewById(message);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("smsto:10001");
+                Intent it = new Intent(Intent.ACTION_SENDTO, uri);
+                it.putExtra("sms_body", "102");
+                mContext.startActivity(it);
+
+            }
+        });
+
+
+
+        convertView.setTag(holder);
+
         holder.nameTextView.setText(map.get(stringArr[position]));
         holder.phoneTextView.setText(phoneArr[position]);
         int idx = position - 1;
