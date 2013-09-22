5ef91871f9c246bbb533dee6c105f02422dd3998
genglei <571618791@qq.com>
2013-09-22 15:00:54 +0800

    Signed-off-by: genglei <571618791@qq.com>添加动态布局

--- src/com/example/slidingmenu/tool/ListAdapter.java
+++ src/com/example/slidingmenu/tool/ListAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.example.slidingmenu.R;
 
 import java.util.HashMap;
@@ -15,22 +16,23 @@ import java.util.Map;
 
 /**
  * Created by Administrator on 13-9-21.
- *
- *
  */
 public class ListAdapter extends BaseAdapter {
 
 
     private LayoutInflater layoutInflater;
     private View.OnClickListener onClickListener;
+    private Context mContext;
     private String[] stringArr;
     private String[] phoneArr;
     private Map<String, String> map = new HashMap<String, String>();
     private Map<String, String> map2 = new HashMap<String, String>();
+    boolean isShow = false;
 
     public ListAdapter(Context context, String[] arr, String[] phoneArr, View.OnClickListener listener, Map<String, String> map, Map<String, String> map2) {
         layoutInflater = LayoutInflater.from(context);
         this.onClickListener = listener;
+        this.mContext = context;
         stringArr = arr;
         this.map = map;
         this.map2 = map2;
@@ -60,39 +62,53 @@ public class ListAdapter extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder = null;
-        if (convertView == null) {
+//        if (convertView == null) {
             convertView = layoutInflater.inflate(R.layout.listitem, null);
+            View view = layoutInflater.inflate(R.layout.addview, null);
+            final LinearLayout tv = (LinearLayout) view.findViewById(R.id.add);
+
+
             final LinearLayout layout = (LinearLayout) convertView.findViewById(R.id.linear);
+            layout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (isShow == false) {
+                        layout.removeView(tv);
+                        layout.addView(tv);
+                        isShow = true;
+
+                    } else if (isShow == true) {
+                        layout.removeView(tv);
+                        isShow = false;
+                    }
+
+
+                }
+            });
 
 
             holder = new ViewHolder();
-//            holder.firstCharHintTextView = (TextView) convertView
-//                    .findViewById(R.id.text_first_char_hint);
+
             holder.nameTextView = (TextView) convertView.findViewById(R.id.text_website_name);
             holder.phoneTextView = (TextView) convertView.findViewById(R.id.text_website_phone);
 
 
             convertView.setTag(holder);
-        } else {
+//        }
+        /*else {
             holder = (ViewHolder) convertView.getTag();
-        }
+        }*/
         holder.nameTextView.setText(map.get(stringArr[position]));
         holder.phoneTextView.setText(phoneArr[position]);
         int idx = position - 1;
         char previewChar = idx >= 0 ? stringArr[idx].charAt(0) : ' ';
         char currentChar = stringArr[position].charAt(0);
-        if (currentChar != previewChar) {
-//            holder.firstCharHintTextView.setVisibility(View.VISIBLE);
-//            holder.firstCharHintTextView.setText(String.valueOf(currentChar));
-        } else {
-            //实例化一个CurrentView后，会被多次赋值并且只有最后一次赋值的position是正确
-//            holder.firstCharHintTextView.setVisibility(View.GONE);
-        }
+
         return convertView;
     }
 
     public final class ViewHolder {
-        public TextView firstCharHintTextView;
+
         public TextView nameTextView;
         private TextView phoneTextView;
 
