8c3e1b68ff0c9ded9af1f44c30109330ae9fb952
unknown <511263942@qq.com>
2013-11-27 20:58:51 +0800

    Signed-off-by: unknown <511263942@qq.com>

--- src/com/example/slidingmenu/fragment/pice/fragment/NewsYuJingPiceFragment1.java
+++ src/com/example/slidingmenu/fragment/pice/fragment/NewsYuJingPiceFragment1.java
@@ -15,33 +15,6 @@
  */
 package com.example.slidingmenu.fragment.pice.fragment;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.Toast;
-
-import com.example.slidingmenu.R;
-import com.example.slidingmenu.tool.Util;
-import com.example.slidingmenu.view.NewsWebView;
-
-import org.jsoup.Jsoup;
-import org.jsoup.select.Elements;
-import org.apache.http.util.ByteArrayBuffer;
-import org.apache.http.util.EncodingUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -50,72 +23,261 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-public class NewsYuJingPiceFragment1 extends Fragment {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
-    Document doc;
+import com.example.slidingmenu.R;
+import com.example.slidingmenu.fragment.pice.fragment.PullDownView.OnPullDownListener;
+import com.example.slidingmenu.tool.Util;
+import com.example.slidingmenu.view.NewsWebView;
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.news_yujing_pice_frag_1, null);
-        if(Util.isNetworkAvailable(NewsYuJingPiceFragment1.this.getActivity())){
-            load(view);
-        }else {
-            Toast.makeText(NewsYuJingPiceFragment1.this.getActivity(), "网络状态不可用！\n请先设置网络", Toast.LENGTH_LONG).show();
-        }
+public class NewsYuJingPiceFragment1 extends Fragment implements
+		OnPullDownListener, OnItemClickListener {
+	private PullDownView mPullDownView;
+	private ListView mListView;
+	Document doc;
+	/** Handler What加载数据完毕 **/
+	private static final int WHAT_DID_LOAD_DATA = 0;
+	/** Handler What更新数据完毕 **/
+	private static final int WHAT_DID_REFRESH = 1;
+	/** Handler What更多数据完毕 **/
+	private static final int WHAT_DID_MORE = 2;
+	protected static final String TAG = "NewsYuJingPiceFragment1";
 
+	private SimpleAdapter mAdapter;
+	private List<String> mStrings = new ArrayList<String>();
+	View view;
 
-        return view;
-    }
+	List<Map<String, String>> list;
 
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		view = inflater.inflate(R.layout.news_yujing_pice_frag_1, null);
+		init();
 
-    protected void load(View view) {
+		mPullDownView = (PullDownView) view
+				.findViewById(R.id.lv_news_yujing_bfxy_frag1);
 
-        try {
-            doc = Jsoup.parse(new URL("http://www.hebeinu.edu.cn/xxxw.asp"), 5000);
-        } catch (MalformedURLException e1) {
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        List<Map<String, String>> list = new ArrayList<Map<String, String>>();
-        Elements es = doc.select("span.newslist a");
-        //html body div#wrapper div#boxout div#center  div#proDownload ul li a
-        //span.newslist a
-        for (Element e : es) {
-            Map<String, String> map = new HashMap<String, String>();
-            String a = e.getElementsByTag("a").text();
-            map.put("title", e.getElementsByTag("a").text());
-            //不显示链接地址
-            map.put("href", "http://www.hebeinu.edu.cn/"
-                    + e.getElementsByTag("a").attr("href"));
-            list.add(map);
-        }
+		mPullDownView.setOnPullDownListener(this);
 
-        ListView listView = (ListView) view.findViewById(R.id.lv_news_yujing_bfxy_frag1);
-        listView.setAdapter(new SimpleAdapter(this.getActivity(), list, android.R.layout.simple_list_item_2,
-                new String[]{"title", "href"}, new int[]{
-                android.R.id.text1, android.R.id.text2
-        }));
-        listView.setOnItemClickListener(onItemClickListener);
+		mListView = mPullDownView.getListView();
 
-    }
+		mListView.setOnItemClickListener(this);
+		mAdapter = new SimpleAdapter(
+				NewsYuJingPiceFragment1.this.getActivity(), list,
+				android.R.layout.simple_list_item_2, new String[] { "title",
+						"href" }, new int[] { android.R.id.text1,
+						android.R.id.text2 });
+		mListView.setAdapter(mAdapter);
+		// 加载数据 本类使用
+		loadData();
 
-    private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+		// 设置可以自动获取更多 滑到最后一个自动获取 改成false将禁用自动获取更多
+		mPullDownView.enableAutoFetchMore(true, 1);
+		// 隐藏 并禁用尾部
+		mPullDownView.setHideFooter();
+		// 显示并启用自动获取更多
+		mPullDownView.setShowFooter();
+		// 隐藏并且禁用头部刷新
+		mPullDownView.setHideHeader();
+		// 显示并且可以使用头部刷新
+		mPullDownView.setShowHeader();
 
-            HashMap<String, Object> map = (HashMap<String, Object>) adapterView.getItemAtPosition(i);
-            Intent intent = new Intent();
-            intent.setClass(NewsYuJingPiceFragment1.this.getActivity(), NewsWebView.class);
-            intent.putExtra("newsid", map.get("href").toString());
-            startActivity(intent);
-        }
-    };
+		return view;
+	}
 
+	private void init() {
+		if (Util.isNetworkAvailable(NewsYuJingPiceFragment1.this.getActivity())) {
+			load();
+		} else {
+			Toast.makeText(NewsYuJingPiceFragment1.this.getActivity(),
+					"网络状态不可用！\n请先设置网络", Toast.LENGTH_LONG).show();
+		}
 
+	}
+
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+	}
+
+	protected void load() {
+
+		try {
+			doc = Jsoup.parse(new URL("http://www.hebeinu.edu.cn/xxxw.asp"),
+					5000);
+		} catch (MalformedURLException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		list = new ArrayList<Map<String, String>>();
+		Elements es = doc.select("span.newslist a");
+		// html body div#wrapper div#boxout div#center div#proDownload ul li a
+		// span.newslist a
+		for (Element e : es) {
+			Map<String, String> map = new HashMap<String, String>();
+			String a = e.getElementsByTag("a").text();
+			map.put("title", e.getElementsByTag("a").text());
+			// 不显示链接地址
+			map.put("href",
+					"http://www.hebeinu.edu.cn/"
+							+ e.getElementsByTag("a").attr("href"));
+			list.add(map);
+		}
+
+	}
+
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		HashMap<String, Object> map = (HashMap<String, Object>) parent
+				.getItemAtPosition(position);
+		Intent intent = new Intent();
+		intent.setClass(NewsYuJingPiceFragment1.this.getActivity(),
+				NewsWebView.class);
+		intent.putExtra("newsid", map.get("href").toString());
+		startActivity(intent);
+		Toast.makeText(this.getActivity(), "啊，你点中我了 " + position,
+				Toast.LENGTH_SHORT).show();
+	}
+
+	/** 刷新事件接口 这里要注意的是获取更多完 要关闭 刷新的进度条RefreshComplete() **/
+	@Override
+	public void onRefresh() {
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// try {
+				// Thread.sleep(2000);
+				// } catch (InterruptedException e) {
+				// e.printStackTrace();
+				// }
+				load();
+				/** 关闭 刷新完毕 ***/
+				mPullDownView.RefreshComplete();// 这个事线程安全的 可看源代码
+
+				Message msg = mUIHandler.obtainMessage(WHAT_DID_REFRESH);
+				// msg.obj = "After refresh " + System.currentTimeMillis();
+				// msg.sendToTarget();
+			}
+		}).start();
+
+	}
+
+	/** 刷新事件接口 这里要注意的是获取更多完 要关闭 更多的进度条 notifyDidMore() **/
+	@Override
+	public void onMore() {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				
+
+				// 告诉它获取更多完毕 这个事线程安全的 可看源代码
+				mPullDownView.notifyDidMore();
+				Message msg = mUIHandler.obtainMessage(WHAT_DID_MORE);
+				// msg.obj = "After more " + System.currentTimeMillis();
+				// msg.sendToTarget();
+			}
+		}).start();
+	}
+
+	private Handler mUIHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case WHAT_DID_LOAD_DATA: {
+				if (msg.obj != null) {
+					List<String> strings = (List<String>) msg.obj;
+					if (!strings.isEmpty()) {
+						mStrings.addAll(strings);
+						mAdapter.notifyDataSetChanged();
+					}
+				}
+				Toast.makeText(NewsYuJingPiceFragment1.this.getActivity(),
+						"" + mListView.getAdapter().getCount(),
+						Toast.LENGTH_LONG).show();
+				// 诉它数据加载完毕;
+				break;
+			}
+			case WHAT_DID_REFRESH: {
+				// String body = (String) msg.obj;
+				// mStrings.add(0, body);
+				// mAdapter.notifyDataSetChanged();
+
+				mAdapter.notifyDataSetChanged();
+				// 告诉它更新完毕
+				break;
+			}
+
+			case WHAT_DID_MORE: {
+//				String body = (String) msg.obj;
+//				mStrings.add(body);
+				Toast.makeText(NewsYuJingPiceFragment1.this.getActivity(), "加载更多", Toast.LENGTH_SHORT).show();
+				
+				Log.i(TAG, "=====================");
+				Elements mores = doc.select("DIV.jt");
+				for (Element more : mores) {
+					
+					String id =  "http://www.hebeinu.edu.cn/"+ more.getElementsByTag("a").attr("href");
+					Log.i(TAG, "id="+id);
+				}
+				mAdapter.notifyDataSetChanged();
+
+				break;
+			}
+			}
+
+		}
+
+	};
+
+	// 模拟数据
+	private String[] mStringArray = { "Abbaye de Belloc",
+			"Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+			"Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu",
+			"Airag", "Airedale", "Aisy Cendre", "Allgauer Emmentaler",
+			"Alverca", "Ambert", "American Cheese" };
+
+	private void loadData() {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {	 
+					Thread.sleep(0000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				List<String> strings = new ArrayList<String>();
+				for (String body : mStringArray) {
+					strings.add(body);
+				}
+				Message msg = mUIHandler.obtainMessage(WHAT_DID_LOAD_DATA);
+				msg.obj = strings;
+				msg.sendToTarget();
+			}
+		}).start();
+		// mPullDownView.onRefreshComplete();
+	}
 }
-
