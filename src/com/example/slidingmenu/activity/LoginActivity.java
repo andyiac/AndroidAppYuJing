06915f2bc7cdd016c75a42358db9add98613c16f
andyiac <andyiaczhang@gmail.com>
2014-05-27 18:25:21 +0800

    add login

--- src/com/example/slidingmenu/activity/LoginActivity.java
+++ src/com/example/slidingmenu/activity/LoginActivity.java
@@ -1,24 +1,44 @@
 package com.example.slidingmenu.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import com.example.slidingmenu.R;
+import com.example.slidingmenu.network.HttpClient;
+import com.example.slidingmenu.network.Request;
+import com.example.slidingmenu.network.mode.LoginoutResponseParam;
+import com.example.slidingmenu.network.mode.RequestParam;
+import com.example.slidingmenu.welcome.MyApplication;
+import com.example.slidingmenu.yujing.client.utils.Utils;
+import org.json.JSONException;
 
 public class LoginActivity extends Activity {
 
-	private Context mContext;
+    private static final boolean D = true;
+    private static final String TAG = "LoginActivity";
+    private Context mContext;
 	private RelativeLayout rl_user;
 	private Button mLogin;
 	private Button register;
+
+
+    private EditText etAccount,etPassword;
+
+    private MyApplication myApplication;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -33,6 +53,8 @@ public class LoginActivity extends Activity {
 		rl_user=(RelativeLayout) findViewById(R.id.rl_user);
 		mLogin=(Button) findViewById(R.id.login);
 		register=(Button) findViewById(R.id.register);
+        etAccount=(EditText)findViewById(R.id.account);
+        etPassword=(EditText)findViewById(R.id.password);
 	}
 
 	private void init(){
@@ -52,10 +74,30 @@ public class LoginActivity extends Activity {
 			Intent intent=new Intent(mContext,NewsYuJingActivity.class);
 			startActivity(intent);
 			finish();
-			
+
+
+		if(TextUtils.isEmpty(etAccount.getText().toString())) {
+			etAccount.setError(getString(R.string.no_empyt_name));
+			return;
+		}
+
+		if(TextUtils.isEmpty(etPassword.getText().toString())) {
+			etPassword.setError(getString(R.string.no_empty_password));
+			return;
+		}
+
+		RequestParam requestParam = new RequestParam();
+		requestParam.setUserName(etAccount.getText().toString());
+		requestParam.setPassword(etPassword.getText().toString());
+		requestParam.setRequestType(requestParam.LOGIN);
+		requestParam.setRandomKey("1234");
+		requestParam.setParams(new String[]{""});
+
+        LoginTask mLoginTask = new LoginTask();
+		mLoginTask.execute(requestParam);
 		}
 	};
-	
+
 	private OnClickListener registerOnClickListener=new OnClickListener() {
 		
 		@Override
@@ -65,4 +107,78 @@ public class LoginActivity extends Activity {
 			
 		}
 	};
+
+
+	/**
+	 * 请求登录的Task
+	 *
+	 */
+	public class LoginTask extends AsyncTask<RequestParam, Integer, Boolean> {
+
+		private ProgressDialog dialog;
+
+		@Override
+		protected void onPreExecute() {
+			dialog = ProgressDialog.show(LoginActivity.this, "",
+					getText(R.string.waiting));
+			super.onPreExecute();
+		}
+
+		@Override
+		protected Boolean doInBackground(RequestParam... param) {
+
+			if(!HttpClient.isConnect(LoginActivity.this)) {
+			if(D) Log.i(TAG, "------1---------");
+			return false;
+			}
+
+			RequestParam requestParam = param[0];
+			String res = Request.request(requestParam.getJSON());
+			//如果请求结果为空字符串，则请求失败
+			if ("".equals(res)) {
+			if(D)	Log.i(TAG,"------2---------");
+				return false;
+			}
+			try {
+				LoginoutResponseParam response = new LoginoutResponseParam(res);
+				if (response.getResult() != LoginoutResponseParam.RESULT_SUCCESS) {
+				if(D) Log.i(TAG,"------3---------"+response.getResult() + LoginoutResponseParam.RESULT_SUCCESS);
+					return false;
+				}
+				// 存储用户登录信息
+				myApplication.setLoginUserInfo(etAccount.getText().toString());
+				SharedPreferences sharedPreferences = myApplication.getLoginUserInfo();
+				SharedPreferences.Editor editor = sharedPreferences.edit();
+				editor.putString(RequestParam.USER_NAME, etPassword.getText().toString());
+				editor.putString(RequestParam.PASSWORD, etPassword.getText().toString());
+				editor.putString(RequestParam.ADDR, response.getPersonAddress());
+				editor.putString(RequestParam.SEX, response.getPersonSex());
+				editor.putString(RequestParam.NAME, response.getPersonName());
+				editor.putString(RequestParam.PHOTO, response.getPersonPhoto());
+				editor.putInt(RequestParam.STATUS, RequestParam.ONLINE);
+				editor.commit();
+				Log.i(TAG,"------4---------");
+
+				return true;
+			} catch (JSONException e) {
+				System.out.println("登陆异常==="+ e.toString());
+				e.printStackTrace();
+			}
+			return false;
+		}
+
+		@Override
+		protected void onPostExecute(Boolean result) {
+
+			dialog.dismiss();
+			if (result) {
+				startActivity(new Intent(LoginActivity.this, NewsYuJingActivity.class));
+				finish();
+			} else {
+				Utils.myToast(LoginActivity.this, getText(R.string.login_fail).toString(), R.drawable.toast_error);
+			}
+			super.onPostExecute(result);
+
+		}
+	}
 }
--- src/com/example/slidingmenu/welcome/MyApplication.java
+++ src/com/example/slidingmenu/welcome/MyApplication.java
@@ -1,20 +1,31 @@
 package com.example.slidingmenu.welcome;
 
 import android.app.Application;
-import android.view.Window;
+import android.content.Context;
+import android.content.SharedPreferences;
+import com.example.slidingmenu.yujing.client.network.mode.RequestParam;
 
 /**
  * Created by andyiac on 13-9-23.
  */
 public class MyApplication  extends Application{
+    private SharedPreferences loginUserInfo;
     @Override
     public void onCreate() {
         super.onCreate();
 
 //        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
+    public SharedPreferences getLoginUserInfo(){
+		SharedPreferences shared = this.getSharedPreferences("lastest_login", Context.MODE_PRIVATE);
+		String name = shared.getString(RequestParam.USER_NAME, "");
+		this.loginUserInfo = this.getSharedPreferences(name, Context.MODE_PRIVATE);
+		return this.loginUserInfo;
+	}
+
+	public void setLoginUserInfo(String name) {
+		SharedPreferences shared = this.getSharedPreferences("lastest_login", Context.MODE_PRIVATE);
+		shared.edit().putString(RequestParam.USER_NAME, name).commit();
+	}
 }
 
-
-
-//---------------test
